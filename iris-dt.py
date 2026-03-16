56a7f4cc41bcadff96706f50204d6472fea1ccbd
Shakil Ur Rehman <sanjaralap@gmail.com>
2026-03-16 16:13:34 +0500

    other guy exp

--- iris-dt.py
+++ iris-dt.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import decision_tree_classifier
 import mlflow
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
@@ -16,7 +16,7 @@ X = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = pd.Series(iris.target, name='target')
 
 max_depth = 3
-n_estimators = 30
+# n_estimators = 30
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -25,17 +25,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Start an MLflow run
 
 with mlflow.start_run():
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
-    model.fit(X_train, y_train)
+    model = decision_tree_classifier(max_depth=max_depth)
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     # Log parameters
-    mlflow.log_param("n_estimators", n_estimators)
+    # mlflow.log_param("n_estimators", n_estimators)
     mlflow.log_param("max_depth", max_depth)
     # Log metrics
     mlflow.log_metric("accuracy", accuracy)
     mlflow.log_artifact(__file__)
     mlflow.sklearn.log_model(model, "model")
-    mlflow.set_tag("author", "shakil")
-    mlflow.set_tag("model_type", "RandomForestClassifier")
+    mlflow.set_tag("author", "Other guy")
+    mlflow.set_tag("model_type", "Decision Tree Classifier")
     print(f"Accuracy: {accuracy}")
